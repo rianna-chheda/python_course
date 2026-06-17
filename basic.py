4f3fa2599e201b9771fc4cc0081a76ad1beb8215
Rianna Chheda <riannachheda@gmail.com>
2026-06-17 13:07:46 +0530

    check1

---
+++ basic.py
@@ -0,0 +1,53 @@
+# syntax -> print()
+print("Hello, World!")
+
+# practice task 
+# convert into comment with cmnd+/
+print("Hello Aditya")
+print("123") 
+print(123) 
+print("abc")  
+print() 
+# print(abc) -> error, no quotation marks	
+print("abc"*10)
+# print(abc*10)-> error, no quotation marks
+print("abc*10")
+print("123"*10)
+print(123*10) 
+print("123*10")
+
+# string -> words, sentence, paragraph, document, alphabet, character ""
+# everything in " " will be a string and will print AS IS 
+
+# syntax -> input is input()
+# syntax -> variable_name = variable
+name = input ("Enter your name: ")
+print("Hello,", name, ",Welcome to Python!")
+
+# ----- example portfolio -----
+print("PORTFOLIO")
+name = input ("Enter your name: ")
+age = input ("Enter your age: ")
+nationality = input("Enter your nationality: ")
+country = input("Enter your country: ")
+birthdate = input("Enter your birthdate (DD/MM/YYYY):")
+hobby = input("Enter your top 2 hobbies: ")
+print()
+print("Hello,", name)
+print(type(name))
+print()
+print("Age:", age)
+print(type(age))
+print()
+print("Nationality:", nationality)
+print(type(nationality))
+print()
+print("Country:", country)
+print(type(country))
+print()
+print("Birthdate:", birthdate)
+print(type(birthdate))
+print()
+print("I love,", hobby, "too")
+print(type(hobby))
+print()
